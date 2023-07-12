8c3554587ff55dd82d575702447e08f88a9b5608
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-07-12 22:39:50 +0900

    add

--- document/docs/.vuepress/config.js
+++ document/docs/.vuepress/config.js
@@ -1,31 +1,13 @@
 const { description } = require('../../package')
 
 module.exports = {
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#title
-   */
-  title: 'Vuepress Docs Boilerplate',
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#description
-   */
+  title: 'LINE LIFF Copilot',
   description: description,
-
-  /**
-   * Extra tags to be injected to the page HTML `<head>`
-   *
-   * ref：https://v1.vuepress.vuejs.org/config/#head
-   */
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
-
-  /**
-   * Theme configuration, here is the default theme configuration for VuePress.
-   *
-   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-   */
   themeConfig: {
     repo: '',
     editLinks: false,
@@ -34,16 +16,8 @@ module.exports = {
     lastUpdated: false,
     nav: [
       {
-        text: 'Guide',
+        text: 'Documantation',
         link: '/guide/',
-      },
-      {
-        text: 'Config',
-        link: '/config/'
-      },
-      {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org'
       }
     ],
     sidebar: {
@@ -59,12 +33,25 @@ module.exports = {
       ],
     }
   },
-
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
-  ]
+  ],
+  // .md ファイルの変更を検知して自動的に再読み込みする設定
+  markdown: {
+    extendMarkdown: (md) => {
+      md.set({ breaks: true })
+      md.renderer.rules.fence = (tokens, idx, options, env, self) => {
+        if (env.VUEPRESS_DEV_SERVER) {
+          const token = tokens[idx]
+          const rawCode = token.content.trim()
+          const code = rawCode.startsWith(':::') ? rawCode : `\`\`\`${token.info}\n${rawCode}\n\`\`\``
+
+          return `<ClientOnly><CustomComponent :code="\`${code}\`" /></ClientOnly>`
+        }
+
+        return self.renderToken(tokens, idx, options)
+      }
+    }
+  }
 }
--- document/docs/README.md
+++ document/docs/README.md
@@ -1,15 +1,14 @@
 ---
 home: true
-heroImage: https://v1.vuepress.vuejs.org/hero.png
-tagline: LINE-LIFF-Copilot&#39;s docs
-actionText: Quick Start →
+tagline: LINE-LIFF-Copilot's docs
+actionText: Start →
 actionLink: /guide/
 features:
-- title: Feature 1 Title
-  details: Feature 1 Description
-- title: Feature 2 Title
-  details: Feature 2 Description
-- title: Feature 3 Title
-  details: Feature 3 Description
-footer: Made by ame / @macl2189 with ❤️
+- title: Fast
+  details: 機能は最低限に抑えられているので高速です。また全てのメソッドに互換性が有る為、全メソッド使用可能です。
+- title: Easy
+  details: 全ては理解が容易くなるような命令規則によってラッピングされています。開発者はロジックに集中出来ます。
+- title: Useful
+  details: 多種多様なメソッドを用意しています。貴方のやりたかった事がこのライブラリによって実現されるでしょう。
+footer: Made by ame / @macl2189 with LINE
 ---
