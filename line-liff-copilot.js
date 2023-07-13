3b3269d8608217e6c1eeedcc4d0c7d45858b84cc
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-07-13 17:30:08 +0900

    Update line-liff-copilot.js

--- line-liff-copilot.js
+++ line-liff-copilot.js
@@ -57,8 +57,8 @@ class LiffCopilot {
         // ユーザーの情報
         this.Info = liff.getDecodedIDToken();
 
-        this.InfoIss = this.Info.iss; // access.line.me 用途不明
-        this.InfoSub = this.Info.sub; // mid風の物 用途不明
+        //this.InfoIss = this.Info.iss; // access.line.me 用途不明
+        //this.InfoSub = this.Info.sub; // mid風の物 用途不明
         //this.InfoAuth = this.Info.amr[0] // lineauthlogin 用途不明
 
         this.Name = this.Info.name; // ユーザーの名前
